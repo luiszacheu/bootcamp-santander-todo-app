3fb4869eaba164c721322f742b2dc1930661cfd3
Luis Gustavo Verri Zacheu <luis.zacheu@santander.com.br>
2021-06-17 07:59:04 -0300

    Adicionando estrutura de métodos que vão interagir com a API.

---
+++ app/src/main/java/bootcamp/snt/bootcampsantandertodo/features/DetailTodoActivity.kt
@@ -0,0 +1,19 @@
+package bootcamp.snt.bootcampsantandertodo.features
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import bootcamp.snt.bootcampsantandertodo.R
+
+class DetailTodoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_todo)
+    }
+
+    fun detailTodo(todoId: Int) {}
+
+    fun uptateTodo(todoId: Int) {}
+
+    fun removeTodo(todoId: Int) {}
+}
---
+++ app/src/main/java/bootcamp/snt/bootcampsantandertodo/features/ListTodosActivity.kt
@@ -0,0 +1,16 @@
+package bootcamp.snt.bootcampsantandertodo.features
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import bootcamp.snt.bootcampsantandertodo.R
+
+class ListTodosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_todo)
+    }
+
+    fun listTodosList() {
+
+    }
+}
